99ebbc194a946f395b0bf642770f58d541bef749
Chali <2024_chali.chen@iticbcn.cat>
2025-02-09 13:08:08 +0100

    INTENTS mini exercici LISTS

--- postgresql_python/read_registre.py
+++ postgresql_python/read_registre.py
@@ -9,9 +9,12 @@ def read_reg():
     conn.commit()
 
     results = cursor.fetchall()
-    print(results)
-    print(results[4][2])
-
+    print(results[9])
+    print(results[9][3])
+    print(results[14])
+    print(results[14][1])
+    print(results[19])
+    print(results[19][4])
     return results
 
 read_reg()
